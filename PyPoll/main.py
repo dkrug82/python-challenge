09fd2a0073077db7f1a31931e57e4d7783a22ee5
Douglas Krug <dkrug82@gmail.com>
2021-04-02 12:39:04 -0400

    added candidates list to PyPoll without duplicates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,19 +13,30 @@ print("-----------------------")
 
 totalVotes = 0
 candidates = []
+candidateVotes = []
 
 with open(election_data, "r", encoding="utf-8") as csvfile:
     csvReader = csv.reader(csvfile,delimiter=",")
     #print(csvReader)
-
+    #candidateVotes = (row[2])
     next(csvfile)
-
+    
+    
+    
     for row in csvReader:
-        totalVotes = totalVotes + 1
+        totalVotes = totalVotes + 1 #for total votes
+        candidateVotes.append(row[2])
 
-    #candidates = row[3]
+    [candidates.append(x) for x in candidateVotes if x not in candidates]
+        #if candidateVotes not in candidates:
+            
+            #candidates.append(candidateVotes)
 
 
-#print(candidates)
+
+    
+
+#print(candidateVotes)
+print(candidates)
 print(f"Total Votes: {totalVotes}")
 print("-----------------------")
